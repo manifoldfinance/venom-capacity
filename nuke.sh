f9532678b6f2d99d1fea49669c288f1f8227b63e
0xJanitor <janitor@manifoldfinance.com>
2022-10-10 05:45:02 +0000

    feat(release): v1.0

--- nuke.sh
+++ nuke.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Nukin Docker Env..."
+docker-compose down --remove-orphans -v --rmi all
 #sudo docker rm --force $(docker ps --all -q)
 #sudo docker rmi --force $(docker images --all -q)
 docker rm -f -v $(docker ps -aq)
